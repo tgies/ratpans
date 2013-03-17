79ce3c3dcdb9b8670e61a2a590bf380c350a5355
Tony Gies <tgies@tgies.net>
2013-03-17 01:27:48 -0500

    initial commit of tarsnap job automator ratpans

---
+++ ratpans.py
@@ -0,0 +1,11 @@
+import yaml
+import time
+from subprocess import call
+
+f = file('jobs.yml', 'r')
+
+for job in yaml.load_all(f):
+    cmdln = ['tarsnap', '--keyfile', '/root/tarsnap/tarsnap-write.key',
+        '-cf', job['name'] + '-' + time.strftime('%Y%m%d')] + job['files']
+    print cmdln
+    # call(cmdln)
