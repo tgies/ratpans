2c6d56f338938640a50aadb156a3bb7013162116
Tony Gies <tgies@tgies.net>
2013-03-17 02:35:38 -0500

    actually here's the version that's not dummied out, oops

--- ratpans.py
+++ ratpans.py
@@ -7,5 +7,4 @@ f = file('jobs.yml', 'r')
 for job in yaml.load_all(f):
     cmdln = ['tarsnap', '--keyfile', '/root/tarsnap/tarsnap-write.key',
         '-cf', job['name'] + '-' + time.strftime('%Y%m%d')] + job['files']
-    print cmdln
-    # call(cmdln)
+    call(cmdln)
