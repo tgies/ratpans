aca886b1d66543a6f2f6404515d503453f55bf9a
Tony Gies <tgies@tgies.net>
2013-03-22 00:20:23 -0500

    new config file feature, new keyfile element in both config and jobs

--- ratpans.py
+++ ratpans.py
@@ -3,14 +3,30 @@ import yaml
 import time
 from subprocess import call
 
+TARSNAP='tarsnap'
+
 print 'ratpans: aight'
-with file('jobs.yml', 'r') as f:
-    jobs = yaml.load_all(f)
+
+with file('ratpans-config.yml', 'r') as c:
+    sc = c.read()
+
+config = yaml.load(sc)
+# TODO: validate config
+
+with file('jobs.yml', 'r') as j:
+    jobs = yaml.load_all(j)
 
     for job in jobs:
+        if 'keyfile' in job:
+            keyfile_option = ['--keyfile', job['keyfile']]
+        elif 'keyfile' in config:
+            keyfile_option = ['--keyfile', config['keyfile']]
+        else:
+            keyfile_option = []
         print 'ratpans: Job ' + job['name'] + ' starting'
-        cmdln = ['tarsnap', '--keyfile', '/root/tarsnap/tarsnap-write.key',
-            '-cf', job['name'] + '-' + time.strftime('%Y%m%d')] + job['files']
+        # TODO: builder class for tarsnap cmdln or class that just abstracts away calling tarsnap probably
+        cmdln = [TARSNAP] + keyfile_option + ['-cf', job['name'] + '-' + time.strftime('%Y%m%d')] + job['files']
+        print cmdln
         ret = call(cmdln)
         if ret == 0:
             print 'ratpans: Job ' + job['name'] + ' completed successfully'
